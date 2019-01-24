4c9f35f0b5a965b8941135bce78b3a9c1a4b6dc1
tsekylekobie <tse.kylekobie@berkeley.edu>
2019-01-23 21:02:51 -0800

    updated code to match new reddit bot

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const NBA = require("nba");
+// var snoowrap = require('snoowrap'); // snoowrap-v1
 var game_threads = [];
 var filtered_comments = [];
 var counter = 0;
@@ -14,62 +14,15 @@ function displayGames() {
     }
 }
 
-function generateTable() {
-    $('#main').empty();
-    game = findGame(this.value);
-    if (game == null || Object.keys(game.links).length == 0) {
-        $('#main').text("No streams found");
-    } else {
-        $("#main").append(tableMaker(game));
-    }
-    $('#game-thread').prop("disabled", false);
-    $('#game-thread').click(() => {
-        window.open("http://www.reddit.com" + game["permalink"]);
-    });
-}
-
-function tableMaker(game) {
-    var table = $('<table></table>');
-    for (var streamer in game.links)
-        table.append(rowMaker(streamer));
-    return table;
-}
-
-function rowMaker(streamer) {
-    row = $('<tr></tr>').append($('<th></th>').text(streamer));
-    for (var j = 0; j < game.links[streamer].length; j++) {
-        link = $('<td></td>').append(
-                $('<a></a>')
-                    .attr({
-                        class: "link",
-                        target: "_blank",
-                        href: game.links[streamer][j]
-                    })
-                    .text("Link " + (j+1)));
-        row.append(link);
-    }
-    return row;
-}
-
-function findGame(title) {
-    for (var i = 0; i < filtered_comments.length; i++) {
-        if (filtered_comments[i].title() == title)
-            return filtered_comments[i];
-    }
-    return null;
-}
-
 // ------BELOW ARE FUNCTIONS TO GET THE STREAM LINKS------
 
 function getGameThreads(res) {
     rdc = res.data.children;
-    
     for (var i = 0; i < rdc.length; i++) {
         if (rdc[i].data.link_flair_css_class == "gamethread" &&
             rdc[i].data.link_flair_text != "Channel Thread")
             game_threads.push(rdc[i]);
     }
-    
     getStreams();
 }
 
@@ -80,25 +33,26 @@ function getStreams() {
         filtered_comments.push(game);
         filterComments(i);
     }
+    $('#main').html(game_threads.length + " games found");
 }
 
 function filterComments(index) {
     reddit.comments(game_threads[index].data.id, "nbastreams").sort("hot").fetch(function(res) {
         comments = res[1].data.children;
-        console.log("Number of comments in thread:" + comments.length);
-        for (var j = 0; j < comments.length; j++) {
-            if ($.inArray(comments[j].data.author, bots) != -1) {
-                filtered_comments[index].comments.push(comments[j]);
-                break;
-            }
-        }
+        // console.log("Number of comments in thread:" + comments.length);
+        // for (var j = 0; j < comments.length; j++) {
+        //     if ($.inArray(comments[j].data.author, bots) != -1) {
+        //         filtered_comments[index].comments.push(comments[j]);
+        //         break;
+        //     }
+        // }
+        filtered_comments[index].comments = res[1].data.children;
         getURL(filtered_comments[index]);
         counter++;
 
         if (counter == filtered_comments.length) {
             displayGames();
             $('#select-game').change(generateTable);
-            $('#main').html(game_threads.length + " games found");
         }
     });
 }
@@ -107,29 +61,29 @@ function getURL(obj) {
     var cmts = obj.comments;
     var links = []
     for (var j = 0; j < cmts.length; j++) {
-        var streams = cmts[j].data.body.match(/(?<=\/u\/).*/g);
-        for (var k = 0; k < streams.length; k++) {
-            var name = streams[k].match(/([^\| ]+)/g)[0]; // Gets the first word
-            var links = streams[k].match(/(?<=\()http.+?(?=\))/g); // Matches anything in parentheses
-            obj.links[name] = links;
+        if ($.inArray(cmts[j].data.author, bots) != -1) {
+            var streams = cmts[j].data.body.match(/(?<=\/u\/).*/g);
+            console.log(streams);
+            for (var k = 0; k < streams.length; k++) {
+                var name = streams[k].match(/([^\| ]+)/g)[0]; // Gets the first word
+                var thread_id = streams[k].match(/(?<=\/comments\/).+?(?=\/)/g)[0];
+                var comment_id = streams[k].match(/[^\/]+?(?=\/\))/g)[0];
+                obj.links[name] = getLinksFromComment(cmts, comment_id);
+            }
         }
     }
 }
 
+function getLinksFromComment(obj, cid) {
+    for (var i in obj) {
+        if (comments[i].data.id == cid) {
+            var body = comments[i].data.body;
+            var links = body.match(/(?<=\()http.+?(?=\))/g); // Matches anything in parentheses
+            return links;
+        }
+    }
+    return [];
+}
+
 // Populate filtered_comments
 reddit.hot('nbastreams').fetch(getGameThreads);
-
-const curry = NBA.findPlayer('Stephen Curry');
-console.log(curry);
-/* logs the following:
-{
-  firstName: 'Stephen',
-  lastName: 'Curry',
-  playerId: 201939,
-  teamId: 1610612744,
-  fullName: 'Stephen Curry',
-  downcaseName: 'stephen curry'
-}
-*/
-params = {gameDate: "12/26/2018"}
-NBA.stats.scoreboard(params).then(console.log);
